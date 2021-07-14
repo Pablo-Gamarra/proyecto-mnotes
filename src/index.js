6388cce4a982b4951a6aab64c3889becd8ee6bdf
Pablo Gamarra <pgamarra27@gmail.com>
2021-07-14 17:26:47 -0300

    Realizamos configuraciones en el archivo principal index.js en la sección SETTINGS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const expresshbs = require('express-handlebars');
+const methodOverride = require('method override');
+const session = require('express-session');
 
 
 //INITIALIZATIONS
@@ -12,28 +14,41 @@ const app = express();
 // Configuración del puerto 3000 o puerto predeterminado en la PC
 app.set('port', process.env.PORT || 3000); 
 
-// Configuración de ruta Carpeta "views"
+// Configuración de ruta carpeta "views"
 app.set('views', path.join(__dirname, 'views'));
 
-// Configuración de handelbars (html)
+// Configuración de "handelbars (html)"
 app.engine('.hbs', expresshbs({
     defaultLayout: main.hbs,
 
-    // Configuración de ruta Carpeta "layouts"
+    // Configuración de ruta carpeta "layouts"
     layoutsDir: path.join(app.get('views'), 'layouts'),
 
-    // Configuración de ruta Carpeta "partials"
+    // Configuración de ruta carpeta "partials"
     partialsDir: path.join(app.get('views'), 'partials'),
     
-    // Configuración de la Extensión ".hbs (Handlebars)"
+    // Configuración de la extensión ".hbs (Handlebars)"
     extname: '.hbs',
 }))
 
 // Configuración del motor de plantillas)
 app.set('view engine', '.hbs');
+
 //--------------------------------------------------------------------------------
 //MIDDLEWARES
+//Método "urlencoded" de "express" para interpretar y recibir datos  ej email y contraseña de usuario
+app.use(express.urlencoded({extended: false}));
 
+//Método "overrride" otros métodos(=/=GET, POST) para enviar datos desde el formulario
+app.use(methodOverride('_method')); 
+
+//Creamos objeto "session" con propiedades configuradas por defecto
+//Sirve para autenticar y almacenar temporalmente al usuario
+app.use(session({
+    secret: 'mysecretapp',
+    resave: true,
+    saveUninitialized: true,
+}));
 
 //--------------------------------------------------------------------------------
 //GLOBAL VARIABLES
